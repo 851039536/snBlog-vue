b71b02b4e4498131bde3631bf719695346e73322
m-Kai.Ouyang <843766158@qq.com>
2023-11-13 12:06:13 +0800

    style: 更正字段

--- src/components/the/footer/components/FooterMouse.vue
+++ src/components/the/footer/components/FooterMouse.vue
@@ -3,8 +3,8 @@ import { useEventKey } from '@hooks/useEventKey'
 import { useUiSetStore } from '@store/modules/uiSettings'
 const ui = useUiSetStore()
 const { addKeyup, removeKeyup } = useEventKey()
-const vi = ref(false)
-const vi2 = ref(true)
+const over = ref(false)
+const leave = ref(true)
 const escape = (e: any) => {
   if (e.key === 'Escape') {
     removeKeyup(escape)
@@ -13,28 +13,28 @@ const escape = (e: any) => {
 
 function handleMouseOver() {
   // 鼠标进入<div>元素时执行的代码
-  vi.value = true
-  vi2.value = false
+  over.value = true
+  leave.value = false
   addKeyup(escape)
 }
 function handleMouseLeave() {
   // 鼠标离开<div>元素时执行的代码
-  vi.value = false
-  vi2.value = true
+  over.value = false
+  leave.value = true
 }
 </script>
 
 <template>
   <div
-    v-if="vi2"
+    v-if="leave"
     i-bytesize-chevron-left
     h-5
     w-5
     class="fixed bottom-[45%] right-0 rounded"
     @mouseover="handleMouseOver"
-    @click="vi = true"></div>
+    @click="over = true"></div>
   <transition name="slidex-fade">
-    <div v-if="vi" class="fixed bottom-[40%] right-0 rounded" @mouseleave="handleMouseLeave">
+    <div v-if="over" class="fixed bottom-[40%] right-0 rounded" @mouseleave="handleMouseLeave">
       <div class="test-cont">
         <div>gitee</div>
         <div>github</div>
